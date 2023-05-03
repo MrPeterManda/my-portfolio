1b660d9f1c16158931fffc0c865b9edc50b9046f
PeterManda19 <peter.pmanda.manda@gmail.com>
2023-05-03 05:45:07 +0200

    drop down removed from About section

--- behaviour.js
+++ behaviour.js
@@ -1,12 +1,1 @@
-var content = document.getElementById('content');
-var seeMore = document.getElementById('see-more');
 
-seeMore.addEventListener('click', function() {
-  if (content.style.height === 'auto') {
-    content.style.height = '200px'; /* set initial height */
-    seeMore.textContent = 'See More';
-  } else {
-    content.style.height = 'auto'; /* set height to auto to show all content */
-    seeMore.textContent = 'See Less';
-  }
-});
